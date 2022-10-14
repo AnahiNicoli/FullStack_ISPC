38bf1ae4905c94760e9360d88a8bf00ac9a30f57
Anahi Nicoli <anahinicoli@gmail.com>
2022-10-13 22:47:51 -0300

    Agrego carrito-modelo

---
+++ Modelos/carrito.py
@@ -0,0 +1,12 @@
+## modelos
+
+## En el modelo vamos a desarrollar las llamadas a la función  
+# (al clic) esa función va al controlador, 
+# este archivo es relativo a cada requerimiento 
+# (cada sección tiene su parte respetando al estructura) 
+# la ruta desde que el usuario hace click en un botón 
+# dispara una llamada a la acción, la ruta inicia en la vista 
+# y el controlador decide lo que sucede después, 
+# dando una respuesta que psa por el modelo, 
+# en el modelo se ejecuta lo que devuelve la vista, 
+# es un ida y vuelta entre vista, controlador, modelo. 
