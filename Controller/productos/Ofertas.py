6486acb08fe7d8f74cf59c3af5bcb613686906fc
Juan Gabriel Soto Valenzuela <99268842+vstecnic@users.noreply.github.com>
2022-10-16 12:15:55 -0300

    Archivo Creado por Analía Lazarte
    Archivo controlador entre carrousel de ofertas (botón de añadir al carrito) y carrito de compras.
    *Lo realizó Analía pero por tener inconvenientes subo yo el archivo*

---
+++ Controller/productos/Ofertas.py
@@ -0,0 +1,12 @@
+## Lo que realiza ésta acción es agregar al carrito, la oferta seleccionada
+
+## cargar carrito
+
+## Agregar oferta al carrito
+
+## Verificar descuento que reste el procentaje segúnn cliente
+
+## Continuar comprando
+
+## Finalizar la compra (pagar)
+
