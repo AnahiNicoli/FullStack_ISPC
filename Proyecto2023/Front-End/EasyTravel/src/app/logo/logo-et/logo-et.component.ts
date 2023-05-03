5259609d91b63c091983b0a4e381fb85a692441e
vstecnic <vstecnic@gmail.com>
2023-05-03 02:45:52 -0300

    angular con carrusel incluido

---
+++ Proyecto2023/Front-End/EasyTravel/src/app/logo/logo-et/logo-et.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-et',
+  templateUrl: './logo-et.component.html',
+  styleUrls: ['./logo-et.component.css']
+})
+export class LogoETComponent {
+
+}
