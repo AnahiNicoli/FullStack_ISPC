8472d6454891ecf8532337e331412c27846dad96
PatriciAleSolis <106406283+PatriciAleSolis@users.noreply.github.com>
2023-05-07 20:26:50 -0300

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, EmpleadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/empleados/empleados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-empleados",
+    templateUrl:"./empleados.component.html",
+    styleUrls:['./empleados.component.css']
+})
+export class EmpleadosComponent{
+    
+}
