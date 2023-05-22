23f06e863a55faf05bf4be52dfd36f58a0aeca9a
vstecnic <vstecnic@gmail.com>
2023-05-21 23:25:58 -0300

    CLASE DNI DJANGO

--- Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/easyTravelApp/models.py
+++ Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/easyTravelApp/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class TIPOS_DNI(models.Model):
+    DNI_ID = models.AutoField(primary_key=True)
+    DNI_ACTIVO= models.BooleanField
+    DNI_DESCRIPTION= models.CharField(max_length=30)
+
+    class Meta:
+        db_table="TIPOS_DNI"
+        verbose_name= "Tipos de DNI"
+        verbose_name_prural= "Tipos de DNI"
+    def _unicode_(self):
+        return self.nombre
+    def _str_(self):
+        return self.nombre
+
+
+
 class Usuarios(models.Model):
     USU_ID = models.AutoField(primary_key=True)
     USU_NOMBRES= models.CharField(max_length=30, blank=False)
@@ -12,6 +28,7 @@ class Usuarios(models.Model):
     USU_MAIL = models.CharField(max_length=30, blank=False)
     USU_CELULAR= models.CharField(max_length=30, blank=False)
     USU_PUNTAJE= models.IntegerField
+    DNI_ID= models.ForeignKey(TIPOS_DNI,to_field='DNI_ID',on_delete=models.CASCADE)
     
     class Meta:
         db_table="Usuario"
