177e72b35e31d4ab05b1b67df8a3f9c3f8ed1fe1
vstecnic <vstecnic@gmail.com>
2023-05-21 22:28:18 -0300

    cambios implementacion en djangobdd

--- Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/Travel_Easy_Django/settings.py
+++ Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/Travel_Easy_Django/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'Travel_Easy_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'easytravel',
+        'USER': 'root',
+        'PASSWORD': 'Terminal02',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
