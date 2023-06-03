721e5e8b64e2e62315aadad1fe318bb3c2f86615
vstecnic <vstecnic@gmail.com>
2023-06-02 21:37:43 -0300

    prueba de botones de perfil

--- Proyecto2023/Front-End/EasyTravel/src/app/app-routing.module.ts
+++ Proyecto2023/Front-End/EasyTravel/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './home/home.module';
+
 
 const routes: Routes = [
   {
     path:'home',
-    loadChildren: () => import('./home/home.module').then (m =>HomeModule)
+    loadChildren: () => import('./home/home.module').then (m =>m.HomeModule)
   }
 
 
--- Proyecto2023/Front-End/EasyTravel/src/app/home/home-routing.module.ts
+++ Proyecto2023/Front-End/EasyTravel/src/app/home/home-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forChild(routes)
+    RouterModule.forChild (routes)
   ]
 })
 export class HomeRoutingModule { }
