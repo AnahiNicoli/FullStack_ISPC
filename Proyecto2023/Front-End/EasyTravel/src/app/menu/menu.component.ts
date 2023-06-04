ef665a10522a8e18a1f678a52533440b608d15be
Anita <anahinicoli@gmail.com>
2023-06-04 11:46:02 -0300

    UserLoginOn OK

--- Proyecto2023/Front-End/EasyTravel/src/app/menu/menu.component.ts
+++ Proyecto2023/Front-End/EasyTravel/src/app/menu/menu.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+  UserLoginOn:boolean=false 
+  constructor () {}
+
+ngOnInit(): void {
+    
+}
 
 }
