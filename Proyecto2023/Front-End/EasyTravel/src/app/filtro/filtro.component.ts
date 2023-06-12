02ebe324832777fe39bbdf45c17649dda7b93477
Anita <anahinicoli@gmail.com>
2023-06-11 21:06:11 -0300

    Componentes, services, observables. Falta ver los datos que NO SE VEN. pero se loguea.

--- Proyecto2023/Front-End/EasyTravel/src/app/filtro/filtro.component.ts
+++ Proyecto2023/Front-End/EasyTravel/src/app/filtro/filtro.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from '../services/auth/login.service';
+import { User } from '../services/auth/user';
 
 @Component({
   selector: 'app-filtro',
   templateUrl: './filtro.component.html',
   styleUrls: ['./filtro.component.css']
 })
-export class FiltroComponent implements OnInit {
+export class FiltroComponent implements OnInit, OnDestroy {
   userLoginOn:boolean=true;
-constructor(){}
+  userData?:User;
+constructor(private loginService:LoginService){}
+
+ngOnDestroy(): void {
+  this.loginService.currentUserData.unsubscribe();
+}
 
 ngOnInit(): void {
-    
+    this.loginService.currentUserLoginOn.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn=userLoginOn;
+      }
+    });
+
+    this.loginService.currentUserData.subscribe({
+      next:(userData) =>{
+        this.userData=this.userData;
+      }
+    })
 }
 }
