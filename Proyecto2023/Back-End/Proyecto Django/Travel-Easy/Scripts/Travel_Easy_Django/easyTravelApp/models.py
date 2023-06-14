0dccaba4600928bdc9f481c5fb3fcaba7754cd19
vstecnic <vstecnic@gmail.com>
2023-06-14 04:02:50 -0300

    agregado clase producto en django

--- Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/easyTravelApp/models.py
+++ Proyecto2023/Back-End/Proyecto Django/Travel-Easy/Scripts/Travel_Easy_Django/easyTravelApp/models.py
@@ -16,19 +16,37 @@ class TIPOS_DNI(models.Model):
         return self.nombre
 
 
+class Producto(models.Model):
+    PROD_ID = models.AutoField(primary_key=True)
+    PROD_NOMBRE= models.CharField(max_length=50, blank=False)
+    PROD_DETALLE= models.CharField(max_length=100, blank=False)
+    PROD_FECHA_INICIO= models.DateField(blank=False)
+    PROD_FECHA_FIN= models.DateField(blank=False)
+    PROD_ACTIVO= models.BooleanField(blank=False)
+    PROD_PRECIO= models.IntegerField(blank=False)
+    
+    class Meta:
+        db_table="Productos"
+        verbose_name= "Productos de EasyTravel"
+        verbose_name_prural= "Productos disponibles en EasyTravel"
+    def _unicode_(self):
+        return self.nombre
+    def _str_(self):
+        return self.nombre
+    
 
-class Usuarios(models.Model):
-    USU_ID = models.AutoField(primary_key=True)
-    USU_NOMBRES= models.CharField(max_length=30, blank=False)
-    USU_APELLIDOS= models.CharField(max_length=30, blank=False)
-    USU_FECH_NAC= models.DateField(blank=False)
-    USU_DNI= models.IntegerField(max_length=30)
-    USU_PASS= models.CharField(max_length=30, blank=False)
-    USU_NPERFIL= models.CharField
-    USU_MAIL = models.CharField(max_length=30, blank=False)
-    USU_CELULAR= models.CharField(max_length=30, blank=False)
-    USU_PUNTAJE= models.IntegerField
-    DNI_ID= models.ForeignKey(TIPOS_DNI,to_field='DNI_ID',on_delete=models.CASCADE)
+    class Usuarios(models.Model):
+        USU_ID= models.AutoField(primary_key=True)
+        USU_NOMBRES= models.CharField(max_length=30, blank=False)
+        USU_APELLIDOS= models.CharField(max_length=30, blank=False)
+        USU_FECH_NAC= models.DateField(blank=False)
+        USU_DNI= models.IntegerField(max_length=30)
+        USU_PASS= models.CharField(max_length=30, blank=False)
+        USU_NPERFIL= models.CharField
+        USU_MAIL = models.CharField(max_length=30, blank=False)
+        USU_CELULAR= models.CharField(max_length=30, blank=False)
+        USU_PUNTAJE= models.IntegerField
+        DNI_ID= models.ForeignKey(TIPOS_DNI,to_field='DNI_ID',on_delete=models.CASCADE)
     
     class Meta:
         db_table="Usuario"
@@ -39,6 +57,7 @@ class Usuarios(models.Model):
     def _str_(self):
         return self.nombre
     
+
 
     class Combos(models.Model):
         T_COMB_ID = models.IntegerField(primary_key=True, blank=False)
