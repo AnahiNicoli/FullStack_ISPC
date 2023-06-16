34e1b0f286c356636d424fb36910f7801871681a
vstecnic <vstecnic@gmail.com>
2023-06-15 21:59:50 -0300

    registro de app en installedApps

--- Proyecto2023/Back-End/EasyTravelDjango/amb_easytravel/EasyTravel/admin.py
+++ Proyecto2023/Back-End/EasyTravelDjango/amb_easytravel/EasyTravel/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Usuarios
+from .models import Categoria
+from .models import Producto
 
 # Register your models here.
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display= ("NOMBRE","APELLIDOS","PUNTAJE")
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display= ("NOMBRE","DETALLE","CANTIDAD")
+class ProductoAdmin(admin.ModelAdmin):
+    list_display= ("NOMBRE","DETALLE","FECHA_INICIO","FECHA_FIN","PRECIO","CANTIDAD")
+
+admin.site.register(Usuarios,UsuariosAdmin)
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Producto,ProductoAdmin)
