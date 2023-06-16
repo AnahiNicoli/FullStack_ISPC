7a68293d4c0be59d26ff8f99b8c1f44577d11c91
vstecnic <vstecnic@gmail.com>
2023-06-15 21:19:59 -0300

    creada app easytravel con tabla usuarios

---
+++ Proyecto2023/Back-End/EasyTravelDjango/amb_easytravel/EasyTravel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EasytravelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'EasyTravel'
---
+++ Proyecto2023/Back-End/EasyTravelDjango/amb_easytravel/EasyTravel/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Usuarios(models.Model):
+    USU_ID= models.AutoField(primary_key=True)
+    USU_NOMBRES= models.CharField(max_length=30, blank=False)
+    USU_APELLIDOS= models.CharField(max_length=30, blank=False)
+    USU_FECH_NAC= models.DateField(blank=False)
+    USU_DNI= models.IntegerField(max_length=30)
+    USU_PASS= models.CharField(max_length=30, blank=False)
+    USU_NPERFIL= models.CharField
+    USU_MAIL = models.CharField(max_length=30, blank=False)
+    USU_CELULAR= models.CharField(max_length=30, blank=False)
+    USU_PUNTAJE= models.IntegerField
+    DNI_ID= models.ForeignKey(TIPOS_DNI,to_field='DNI_ID',on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table="Usuario"
+        verbose_name= "Usuario de EasyTravel"
+        verbose_name_prural= "Usuarios creados de EasyTravel"
+    def _unicode_(self):
+        return self.nombre
+    def _str_(self):
+        return self.nombre
