679bb30ae27cbd682e4d9ebdcf22dd1a7cdb1840
vstecnic <vstecnic@gmail.com>
2023-06-19 20:19:06 -0300

    conf. de installedApps en django

--- Proyecto2023/Back-End/abm_et/abm_et/settings.py
+++ Proyecto2023/Back-End/abm_et/abm_et/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "corsheaders",
+    'django_rest_passwordreset',
     'easyTravel',
+    'easyTravel.app.EasytravelConfig',
+
 ]
 
 MIDDLEWARE = [
